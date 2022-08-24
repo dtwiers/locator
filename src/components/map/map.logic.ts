eaa23644752f99d82ce60c0e4a8864180d5a9c80
Derek Wiers <dwiers@exofreight.com>
2022-08-24 17:27:19 -0500

    styles; recentered on des moines airport

--- src/components/map/map.logic.ts
+++ src/components/map/map.logic.ts
@@ -1,13 +1,16 @@
 import { Map } from 'mapbox-gl';
 import { MAPBOX_TOKEN } from '../../config/env';
-import 'mapbox-gl/dist/mapbox-gl.css'
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 export const mountMap2 = (element: HTMLElement) => {
   const myMap = new Map({
     container: element,
-    center: [-74.5, 40],
+    center: [-93.663101196, 41.534000],
+    zoom: 13,
     // projection: { name: 'albers' },
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/mapbox/dark-v10',
+    // style: 'mapbox://styles/mapbox/outdoors-v11',
+    // style: 'mapbox://styles/mapbox/satellite-streets-v11',
     accessToken: MAPBOX_TOKEN
   });
 };
